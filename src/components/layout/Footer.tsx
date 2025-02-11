3db37a09ead0e48f47890c4515aa61f88be22f3b
Emanuel-Marques <70699733+Emanuel-Marques@users.noreply.github.com>
2025-02-11 10:51:11 +0100

    :sparkles: feat: componente footer

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return(
+        <footer className="text-dark-50 font-inter bg-dark-10 text-center font-semibold">
+            &copy; 2025 <span>Todos os direitos reservados</span>
+        </footer>
+    )
+}
